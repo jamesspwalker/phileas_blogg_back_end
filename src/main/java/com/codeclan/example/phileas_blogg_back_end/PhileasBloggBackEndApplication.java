f4e361019d76661e363c972df797290f9cc37bba
Ruairidh Holwill <roore@hotmail.co.uk>
2019-07-05 13:56:25 +0100

    initial back end commit

---
+++ src/main/java/com/codeclan/example/phileas_blogg_back_end/PhileasBloggBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.phileas_blogg_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhileasBloggBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhileasBloggBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeclan/example/phileas_blogg_back_end/PhileasBloggBackEndApplicationTests.java
@@ -0,0 +1,16 @@
+package com.codeclan.example.phileas_blogg_back_end;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PhileasBloggBackEndApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
