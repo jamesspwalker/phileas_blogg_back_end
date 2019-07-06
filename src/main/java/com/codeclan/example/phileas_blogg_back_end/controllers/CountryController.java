bd1885ac0fddbd358bd8ef7fb34492b8db7bca46
Ruairidh Holwill <roore@hotmail.co.uk>
2019-07-06 11:44:04 +0100

    Can get all countries

--- src/main/java/com/codeclan/example/phileas_blogg_back_end/controllers/CountryController.java
+++ src/main/java/com/codeclan/example/phileas_blogg_back_end/controllers/CountryController.java
@@ -1,9 +1,22 @@
 package com.codeclan.example.phileas_blogg_back_end.controllers;
 
+import com.codeclan.example.phileas_blogg_back_end.models.Review;
+import com.codeclan.example.phileas_blogg_back_end.repositories.CountryRepository.CountryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @CrossOrigin
+@RestController
+@RequestMapping(value="/countries")
 public class CountryController {
+
+    @Autowired
+    CountryRepository countryRepository;
 
 
 }
