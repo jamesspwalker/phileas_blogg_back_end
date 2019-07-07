bfd732fe5eda7503484ede0e917a956671fad787
James <james___3248@hotmail.com>
2019-07-07 13:54:25 +0100

    can add users to db

--- src/main/java/com/codeclan/example/phileas_blogg_back_end/controllers/ReviewController.java
+++ src/main/java/com/codeclan/example/phileas_blogg_back_end/controllers/ReviewController.java
@@ -11,7 +11,7 @@ import java.util.List;
 @CrossOrigin
 @RestController
 @RequestMapping(value="/reviews")
-public class ReviewController {
+public class  ReviewController {
 
     @Autowired
     ReviewRepository reviewRepository;
