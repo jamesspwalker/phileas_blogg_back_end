5162cea7ee45c33aee4e1a71dbc9fa3c2b929eed
Ruairidh Holwill <roore@hotmail.co.uk>
2019-07-09 19:10:12 +0100

    changed dataloader

--- src/main/java/com/codeclan/example/phileas_blogg_back_end/repositories/CountryRepository/CountryRepository.java
+++ src/main/java/com/codeclan/example/phileas_blogg_back_end/repositories/CountryRepository/CountryRepository.java
@@ -5,7 +5,6 @@ import com.codeclan.example.phileas_blogg_back_end.models.Country;
 import com.codeclan.example.phileas_blogg_back_end.models.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
-
 import java.util.List;
 
 @CrossOrigin
